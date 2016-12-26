53e66b0ffcb8712d7c43e96d9a4dbde1f99aa266
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2016-12-26 15:38:23 +0900

    test a combination of Ubuntu and jdk7 only (#5)
    * typo
    
    * add openjdk-7-jdk to ubuntu test case
    
    to see two jdk can coexist
    
    * test idempotency_test when openjdk7
    
    * fix wrong file name
    
    * update README
    
    * fix the condition in the task
    
    there needs to be a filter that returns true when a element of a list
    matches regex.
    
    * typo
    
    * workaround ansible-role-logstash #7
    
    https://github.com/reallyenglish/ansible-role-logstash/issues/7
    
    * support ubuntu 16.04
    
    fixes #4
    
    * fix examples for ubuntu
    
    * the role now supports 16.04

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -11,7 +11,11 @@ when "centos"
 when "openbsd"
   packages = [ "jdk-1.7.0.80p1v0" ]
 when "ubuntu"
-  packages = [ "oracle-java8-installer" ]
+  if os[:release].to_f < 16.04
+    packages = [ "oracle-java8-installer", "openjdk-7-jdk" ]
+  else
+    packages = [ "openjdk-8-jdk" ]
+  end
 end
 
 packages.each do |package|
@@ -20,13 +24,31 @@ packages.each do |package|
   end
 end
 
-case os["family"]
+case os[:family]
 when "freebsd"
-  describe "/proc" do
-    it { should be_mounted.with(:type => "procfs") }
+  describe file("/proc") do
+    # XXX https://github.com/reallyenglish/ansible-role-logstash/issues/7
+    # it { should be_mounted.with(:type => "procfs") }
+    it { should be_mounted }
   end
 
-  describe "/dev/fd" do
-    it { shoulld be_mounted.with(:type => "fdescfs") }
+  describe file("/dev/fd") do
+    # XXX https://github.com/reallyenglish/ansible-role-logstash/issues/7
+    # it { should be_mounted.with(:type => "fdescfs") }
+    it { should be_mounted }
+  end
+when "ubuntu"
+  if os[:release].to_f < 16.04
+    describe command("debconf-show oracle-java8-installer") do
+      its(:stdout) { should match(/#{ Regexp.escape("* shared/accepted-oracle-license-v1-1: true") }/) }
+      its(:stderr) { should match(/^$/) }
+      its(:exit_status) { should eq 0 }
+    end
+  else
+    describe command("debconf-show oracle-java8-installer") do
+      its(:stdout) { should match(/^$/) }
+      its(:stderr) { should match(/^$/) }
+      its(:exit_status) { should eq 0 }
+    end
   end
 end
